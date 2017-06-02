f403d51cdafbb00528fafd86c220f965584268df
Stephen Sorensen <shuoink@gmail.com>
2017-06-01 22:37:48 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const stripUnnecessaryBrackets = (open = '[', close = ']', join = [',']) =>
+  s => {
+    const buffers = [{value: '', joined: false}];
+    for (let i = 0; i < s.length; i++) {
+      switch (s[i]) {
+        case open:
+          buffers.push({value: '', joined: false});
+          break;
+        case close: {
+          const buffer = buffers.pop();
+          if (buffer.joined && buffers.length !== 1) {
+            buffers[buffers.length - 1].value += `${open}${buffer.value}${close}`;
+          } else {
+            buffers[buffers.length - 1].value += buffer.value;
+          }
+          break;
+        }
+        default:
+          buffers[buffers.length - 1].value += s[i];
+          if (join.includes(s[i])) {
+            buffers[buffers.length - 1].joined = true;
+          }
+      }
+    }
+    return buffers.map(b => b.value).join('');
+  };
+
+module.exports = stripUnnecessaryBrackets;
