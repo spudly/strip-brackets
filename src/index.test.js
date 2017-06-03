cd6d44f939ed376b814a43ee25bdb8c84ed544c6
Stephen Sorensen <shuoink@gmail.com>
2017-06-02 20:41:37 -0400

    fix(tests)

--- src/index.test.js
+++ src/index.test.js
@@ -2,12 +2,12 @@ const stripBrackets = require('./index.js');
 
 test('strips brackets', () => {
   expect(stripBrackets()('[[a],[b]]')).toBe('a,b');
-  expect(stripBrackets()('[[a,b]]')).toBe('a,b');
+  expect(stripBrackets()('[a,[b,c]]')).toBe('a,[b,c]');
   expect(stripBrackets()('[a,b]')).toBe('a,b');
 });
 
 test('strips parens', () => {
   expect(stripBrackets('(', ')', [','])('((a),(b))')).toBe('a,b');
-  expect(stripBrackets('(', ')', [','])('((a,b))')).toBe('a,b');
+  expect(stripBrackets('(', ')', [','])('(a,(b,c))')).toBe('a,(b,c)');
   expect(stripBrackets('(', ')', [','])('(a,b)')).toBe('a,b');
 });
